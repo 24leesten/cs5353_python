7fa642aea762401fed832fb00dcdcaf609f6ff7b
24leesten <l.stenquist@utah.edu>
2016-09-27 18:53:08 -0600

    readme and runs.sh added

---
+++ 02Assignment/run.sh
@@ -0,0 +1,10 @@
+echo ''
+echo '----------------------'
+echo '| ASSIGNMENT 02      |'
+echo '|	  Leland Stenquist |'
+echo '|	  U0634909         |'
+echo '----------------------'
+echo ''
+python 01_Experiment.py
+python 02_Experiment.py
+python 03_Experiment.py
